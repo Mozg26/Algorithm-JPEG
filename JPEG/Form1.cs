3511c3d13e94147d6d4b3146d91bbff2b54e3166
Mozg26 <92916665+Mozg26@users.noreply.github.com>
2022-02-17 20:21:45 +0300

    Add files via upload

---
+++ JPEG/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JPEG
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Bitmap img = (Bitmap)pictureBox1.Image;
+            JPEGimage obj = new JPEGimage(img);
+
+            double[,,] matrixRGB;
+            matrixRGB = obj.CreateMatrixes();
+
+            double[,,] matrixYCbCr;
+            matrixYCbCr = obj.FromRGBtoYCbCr(matrixRGB);
+
+            double[,,] NewMatrixRGB = new double[obj.Height, img.Width, 3];
+
+            NewMatrixRGB = obj.FromYCbCrtoRGB(NewMatrixRGB, matrixYCbCr);
+
+            obj.img2 = obj.CreateBitmap(obj.img2, NewMatrixRGB);
+            pictureBox2.Image = obj.img2;
+    }
+    }
+}
---
+++ JPEG/JPEGimage.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace JPEG
+{
+    class JPEGimage
+    {
+        public Bitmap img1;
+        public Bitmap img2;
+        public int Height;
+        public int Width;
+
+        public JPEGimage(Bitmap img)
+        {
+            img1 = img;
+            img2 = null;
+            Height = img.Height;
+            Width = img.Height;
+        }
+
+        public double[,,] CreateMatrixes() //создаём трёхмерный массив пикселей (по цветам)
+        {
+            int i, j;
+
+            Color pixels;
+            double[,,] matrixRGB = new double[Height, Width, 3];
+
+            for (i = 0; i < Height; ++i)
+                for (j = 0; j < Width; ++j)
+                {
+                    pixels = img1.GetPixel(j, i);
+
+                    matrixRGB[i, j, 0] = pixels.R;
+                    matrixRGB[i, j, 1] = pixels.G;
+                    matrixRGB[i, j, 2] = pixels.B;
+                }
+
+            return matrixRGB;
+        }
+
+        public double[,,] FromRGBtoYCbCr(double[,,] matrixRGB)
+        {
+            int i, j;
+            double[,,] matrixYCbCr = new double[Height, Width, 3];
+
+            for (i = 0; i < Height; ++i)
+                for (j = 0; j < Width; ++j)
+                {
+
+                    matrixYCbCr[i, j, 0] = 0.299 * matrixRGB[i, j, 0] + 0.587 * matrixRGB[i, j, 1] + 0.114 * matrixRGB[i, j, 2];
+                    matrixYCbCr[i, j, 1] = 128 - 0.1687 * matrixRGB[i, j, 0] - 0.3313 * matrixRGB[i, j, 1] + 0.5 * matrixRGB[i, j, 2];
+                    matrixYCbCr[i, j, 2] = 128 + 0.5 * matrixRGB[i, j, 0] - 0.4187 * matrixRGB[i, j, 1] - 0.0813 * matrixRGB[i, j, 2];
+                }
+
+            return matrixYCbCr;
+        }
+
+        public double[,,] FromYCbCrtoRGB(double[,,] MatrixRGB, double[,,] matrixYCbCr)
+        {
+            int i, j, N = 8;
+
+            for (i = 0; i < Height; ++i)
+                for (j = 0; j < Width; ++j)
+                {
+                    MatrixRGB[i, j, 0] = matrixYCbCr[i, j, 0] + 1.402 * (matrixYCbCr[i, j, 2] - 128);
+                    MatrixRGB[i, j, 1] = matrixYCbCr[i, j, 0] - 0.34414 * (matrixYCbCr[i, j, 1] - 128) - 0.71414 * (matrixYCbCr[i, j, 2] - 128);
+                    MatrixRGB[i, j, 2] = matrixYCbCr[i, j, 0] + 1.772 * (matrixYCbCr[i, j, 1] - 128);
+                }
+
+            return MatrixRGB;
+        }
+
+        public Bitmap CreateBitmap(Bitmap img, double[,,] matrixRGB)
+        {
+            int i, j;
+
+            img = new Bitmap(Height, Width);
+
+            for (i = 0; i < Height; ++i)
+                for (j = 0; j < Width; ++j)
+                {
+                    if (matrixRGB[i, j, 0] < 0) matrixRGB[i, j, 0] = 0;
+                    if (matrixRGB[i, j, 1] < 0) matrixRGB[i, j, 1] = 0;
+                    if (matrixRGB[i, j, 2] < 0) matrixRGB[i, j, 2] = 0;
+
+                    img.SetPixel(j, i, Color.FromArgb((byte)matrixRGB[i, j, 0], (byte)matrixRGB[i, j, 1], (byte)matrixRGB[i, j, 2]));
+                }
+
+            return img;
+        }
+
+
+    }
+}
